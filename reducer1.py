15dbd14a85521071c0802606c5b8f5c5220fda82
Raymond Karim Roberts <raymond.k.roberts@gmail.com>
2018-10-06 18:06:06 -0500

    Update reducer1.py
    First attempt to run in Hadoop

--- reducer1.py
+++ reducer1.py
@@ -5,8 +5,6 @@ import sys
  
 def main(argv):
   
-  #Testing off of Jordanias Minicomp
-  
   #create dictionary storing the month, country, and customer ID as the collective key value, and the purchance Amount currently being read in
   MCI_Dict = {}
   
@@ -18,55 +16,45 @@ def main(argv):
     monthCountryID, purchaseAmount = line.split('\t', 1)
     monthCountry, customerID = monthCountryID.split(':',1)
 	month, country = monthCountry.split(',',1)
-	
-	month
-	
 	purchaseAmount= float(purchaseAmount)
     
 	
-	#update key value pairs as neceessary given current line read in
+	# update key value pairs for the unique month-country-customerID key and the total purchaseTally for that customer for that country for that month as the value. If the key is not in the dictionary
+	# it is added, and initialized with the current line read in amount
 	
 	if monthCountryID in MCI_Dict:
 	  MCI_Dict[monthCountryID] += purchaseAmount
-	  purchaseTally = MCI_Dict[monthCountryID]
 	else:
-	  MCI_Dict.update({monthCountryID : purchaseAmount}]
-	  purchaseTally = MCI_Dict[monthCountryID]
+	  MCI_Dict.update({monthCountryID : purchaseAmount})
+	
+	# assign value for the current tally amount for that unique Customer ID for that unique month-country combination
+	purchaseTally = MCI_Dict[monthCountryID]
 	  
-	  
-	# maintain record of the highest expenditure tally for a month and country combination. If the current customer tally is the highest, print the monthCountryID
+	# updating dictionary that tracks the months for each country as the key, and the high expenditure amount.  Once the key-value pair has been updated, if the list of customer ID's with the highest expenditure amount
+	# for that pair has changed, it is printed out. This results in a 'rolling basis' updating and printing of the highest spenders for each month and country combination. 
+	
+	
 	
 	if monthCountry in MCSaleDict:
-	  if purchaseTally > MCSaleDict[monthCountry]:
-	    
-		######## Needs to add the customer ID as well
-		customerIDStackStack = ":" + customerID
-		MCSaleDict[monthCountry] = [purchaseTally, customerIDStack]
-		
-	  else if  purchaseTally == MCSaleDict[monthCountry]:
+	
+	  rawHighInfo = MCSaleDict[monthCountry]
+	  highExpenditureAmount, previousID = rawhighInfo.split(':',1)
+	  highExpenditureAmount = float(highExpenditureAmount)
+	  
+	  if purchaseTally >  highExpenditureAmount:
+		highAmountWithCustomerIDs = str(purchaseTally) + ":" + customerID
+		MCSaleDict[monthCountry] = [highAmountWithCustomerIDs]
+		print(month, ",", country, ":", customerID))
+	  else if  purchaseTally ==  highExpenditureAmount:
 	    MCSaleDict[monthCountry] = MCSaleDict[monthCountry] + "," + customerID
-		MCI_Dict.update({monthCountryID : purchaseAmount}]
-		highpurchase = MCI_Dict[monthCountryID]  
-		
-		print(    key and value   )
+		listedID = previousID + ',' + customerID
+	    print(month, ",", country, ":", listedID))
 	else:
-	  MCI_Dict.update({monthCountryID : purchaseAmount}]
+	  highAmountWithCustomerIDs = str(purchaseTally) + ":" + customerID
+	  MCSaleDict.update({monthCountryID : highAmountWithCustomerIDs})
+	  print(month, ",", country, ":", customerID))
 	  
 	
-	
-	
-	
-	
-	
-	#where I left off, I am sleepy
-	
-      if current_customerMonth:
-        print('%s\t%s' % (current_word, current_count))
-      current_count = count
-      current_word = word
-  if current_word == word:
-    print('%s\t%s' % (current_word, current_count))
-
 #Note there are two underscores around name and main
 if __name__ == "__main__":
   main(sys.argv)
